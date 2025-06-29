8391c5cfffe91ac906f1b140b3d2ae4524739e07
StrangePineAplle <shadow77777777@mail.ru>
2025-06-29 19:38:13 +0300

    change company

--- AI.py
+++ AI.py
@@ -5,42 +5,46 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.chains import LLMChain
 import warnings
 import httpx
-warnings.filterwarnings('ignore')
 
+warnings.filterwarnings('ignore')
 client = httpx.Client(timeout=httpx.Timeout(10.0))
 
 class AI:
-    def __init__(self, pathToData = "./data/data_x.txt", key = None):
-        if key == None: key = "ZjdiZmQzZDktNWRmMC00MGZhLTk4ZTItNDMzNTk4YTNkODcxOjlmYjYzNTQwLTZiNTMtNGUyNy1iZTY2LTUyYzI3YmNlNjE1MQ=="
+    def __init__(self, pathToData = "./data/company_data.txt", key = None):
+        if key == None: 
+            key = "ZDVkODE5NjctMTJiZS00ZmE1LWI1ZGItNGRiYjg4OWJkMjQ1OjdlMjMwZjUyLWRlMzItNGE4NS05YzcwLWYyMDBiZjk5N2IwZg=="
+        
         self.pathToData = pathToData
-
-        auth = key #ключь доступа
+        auth = key  # ключ доступа к GigaChat
+        
         giga = GigaChat(credentials=auth,
-                        model='GigaChat:latest',
-                        verify_ssl_certs=False
-                        )
-
-        loader = TextLoader(self.pathToData, encoding = 'UTF-8') # загружаем файл для пересказа
+                       model='GigaChat:latest',
+                       verify_ssl_certs=False)
+        
+        # Загружаем корпоративные данные ТехСтрим
+        loader = TextLoader(self.pathToData, encoding = 'UTF-8')
         self.documents = loader.load()
-
-        text_splitter = RecursiveCharacterTextSplitter( # дробим на чанки
+        
+        text_splitter = RecursiveCharacterTextSplitter(  # дробим на чанки
             chunk_size = 1000,
-            chunk_overlap  = 0,
+            chunk_overlap = 0,
             length_function = len,
             is_separator_regex = False,
         )
-
+        
         self.documents = text_splitter.split_documents(self.documents)
-        bonch_promt =  hub.pull("moneco/bonch_promt")
-        self.chain = LLMChain(prompt=bonch_promt, llm=giga)
-
-
-    def askAI(self,chain = None, documents = None, input = 'Кто ты?'):
+        
+        # Используем промпт для корпоративного ассистента
+        techstream_prompt = hub.pull("moneco/techstream_corporate_assistant")
+        self.chain = LLMChain(prompt=techstream_prompt, llm=giga)
+    
+    def askAI(self, chain = None, documents = None, input = 'Расскажи о компании ТехСтрим'):
         if chain == None: chain = self.chain
         if documents == None: documents = self.documents
-
-        res = chain.invoke({ # обращаемся к нейронке
-        "input_documents": documents,
-        "question" : input
+        
+        res = chain.invoke({  # обращаемся к нейронке
+            "input_documents": documents,
+            "question" : input
         })
+        
         return res["text"]
--- Telegram.py
+++ Telegram.py
@@ -15,9 +15,7 @@ load_dotenv()
 token = os.getenv("TOKEN")
 bot = telebot.TeleBot(token=token)
 
-
 user_states = {}
-
 # Словарь для хранения состояния кнопок
 button_states = {}
 mistake = False
@@ -37,7 +35,7 @@ def check_email(email):
         port = 587
         return "smtp.zoho.com", port
     else:
-        None, None
+        return None, None
 
 def send_email(chat_id, email, password, message):
     email_type, port = check_email(email)
@@ -45,15 +43,15 @@ def send_email(chat_id, email, password, message):
         getter = email
         sender = email
         password = password
-
+        
         msg = MIMEMultipart()
-        subject = "Сообщение из телеграмм-бота"
+        subject = "Запрос из корпоративного бота ТехСтрим"
         msg['Subject'] = Header(subject, 'utf-8')
         msg['From'] = sender
         msg['To'] = getter
-
+        
         msg.attach(MIMEText(message, 'plain', 'utf-8'))
-
+        
         server = smtplib.SMTP(email_type, port)
         server.ehlo()
         server.starttls()
@@ -61,20 +59,23 @@ def send_email(chat_id, email, password, message):
         server.login(sender, password)
         server.sendmail(sender, getter, msg.as_string())
         server.quit()
-
+        
         bot.send_message(chat_id, "Сообщение успешно отправлено")
 
 @bot.message_handler(commands=['start'])
 def start(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     item1 = types.KeyboardButton('/start')
-    item2 = types.KeyboardButton('Разработчики')
-    item3 = types.KeyboardButton('Нашел Ошибку')
-    item4 = types.KeyboardButton('Помощь')
+    item2 = types.KeyboardButton('Служба поддержки')
+    item3 = types.KeyboardButton('Сообщить об ошибке')
+    item4 = types.KeyboardButton('Справка')
+    
     markup.add(item1, item2, item3, item4)
     button_states[message.from_user.id] = markup
-
-    bot.send_message(message.from_user.id, "Привет, {0.first_name}! Я телеграм бот BAI, и я здесь, чтобы сделать вашу студенческую жизнь проще.".format(message.from_user), reply_markup=markup)
+    
+    bot.send_message(message.from_user.id, 
+        "Добро пожаловать, {0.first_name}! Я корпоративный ассистент ТехСтрим AI, и я здесь, чтобы помочь с информацией о нашей компании.".format(message.from_user), 
+        reply_markup=markup)
 
 @bot.message_handler(content_types=['text'])
 def handle_text(message):
@@ -82,30 +83,43 @@ def handle_text(message):
     chat_id = message.chat.id
     markup = button_states.get(message.from_user.id, None)
     text = message.text
+    
     email = os.getenv("EMAIL")
     password = os.getenv("PASS")
+    
     if markup:
-        if text == 'Нашел Ошибку':
+        if text == 'Сообщить об ошибке':
             mistake = True
-            bot.send_message(message.from_user.id, "Напишите найденную неточность одним сообщением", reply_markup=markup)
-        elif text == "Помощь":
-            bot.send_message(message.from_user.id, "Данный бот разрабатывался с целью упрощения получения информации об высшем учебном заведении СПБГУТ имени М.А.Бонч-Бруевиче, преподавателях и студенческом совете. Вы можете задать любой интересующий вас вопрос. В случае каких-либо ошибок или неточности полученной информации вы можете описать свою проблему и отправить разработчикам, выбрав 'Разработчики' или 'Нашел Ошибку'", reply_markup=markup)
-        elif text == 'Разработчики':
-            bot.send_message(chat_id, "Введите ваше пожелание/замечание одним сообщением в формате /mess ваше сообщение", reply_markup=markup)
-        elif "/mess" in text:
-            text = text.replace("/mess ", "")
-            send_email(chat_id=chat_id,email=email,password=password,message=text)
+            bot.send_message(message.from_user.id, 
+                "Опишите найденную проблему или неточность одним сообщением", 
+                reply_markup=markup)
+        elif text == "Справка":
+            bot.send_message(message.from_user.id, 
+                "Данный бот разработан для предоставления информации о IT-компании ТехСтрим, наших услугах, департаментах и сотрудниках. Вы можете задать любой вопрос о компании. В случае обнаружения ошибок или неточностей, используйте функции 'Служба поддержки' или 'Сообщить об ошибке'", 
+                reply_markup=markup)
+        elif text == 'Служба поддержки':
+            bot.send_message(chat_id, 
+                "Введите ваш запрос или обращение одним сообщением в формате /support ваше сообщение", 
+                reply_markup=markup)
+        elif "/support" in text:
+            text = text.replace("/support ", "")
+            send_email(chat_id=chat_id, email=email, password=password, message=text)
         else:
             if mistake:
                 current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-                with open("./logs/errorList.txt", "a") as file:
+                with open("./logs/error_reports.txt", "a") as file:
                     file.write(f"\n{message.from_user.first_name} | {current_time} | {message.text}")
-                bot.send_message(message.from_user.id, "Ваша информация будет обработана в ближайшее время", reply_markup=markup)
+                bot.send_message(message.from_user.id, 
+                    "Спасибо за обращение. Ваша информация будет обработана службой поддержки в ближайшее время", 
+                    reply_markup=markup)
                 mistake = False
             else:
                 chatAI = AI()
-                bot.send_message(message.from_user.id, chatAI.askAI(input=message.text), reply_markup=markup)          
+                bot.send_message(message.from_user.id, 
+                    chatAI.askAI(input=message.text), 
+                    reply_markup=markup)
     else:
-        bot.send_message(message.from_user.id, "Произошла ошибка. Пожалуйста, начните заново с команды /start.")
+        bot.send_message(message.from_user.id, 
+            "Произошла системная ошибка. Пожалуйста, начните заново с команды /start.")
 
 bot.polling(none_stop=True, interval=0)
